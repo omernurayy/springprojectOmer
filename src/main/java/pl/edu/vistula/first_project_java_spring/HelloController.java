12cb10d6bc9f701db7e55983fc56dbf808964278
Ömer Nuray <161148003+omernurayy@users.noreply.github.com>
2024-06-07 13:39:30 +0200

    Update HelloController.java

--- src/main/java/pl/edu/vistula/first_project_java_spring/HelloController.java
+++ src/main/java/pl/edu/vistula/first_project_java_spring/HelloController.java
@@ -4,13 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="Vistula") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
+	@GetMapping(value = "/")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello Vistula, in my first Spring controller.";
+	}
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="Vistula") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
 }
